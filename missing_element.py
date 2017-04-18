d5158cd6857584c023440b974a5ab033e64f0997
Jon Baird <jon.baird@northstate.net>
2017-04-17 21:51:47 -0400

    17 April 2017

--- missing_element.py
+++ missing_element.py
@@ -2,6 +2,25 @@
 """
 Find the missing element
 """
+from nose.tools import assert_equal
+
+
+class TestFinder(object):
+    """
+    Test Class
+    """
+
+    def test(self, sol):
+        """
+        
+        :param sol: 
+        :return: 
+        """
+        assert_equal(sol([5, 5, 7, 7], [5, 7, 7]), 5)
+        assert_equal(sol([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6]), 5)
+        assert_equal(sol([9, 8, 7, 6, 5, 4, 3, 2, 1],
+                         [9, 8, 7, 5, 4, 3, 2, 1]), 6)
+        print("PASS")
 
 
 def finder(arr1, arr2):
@@ -10,6 +29,12 @@ def finder(arr1, arr2):
     :param arr2: shuffled array with random element missing
     :return: missing element
     """
-    a = set(arr1)
-    b = set(arr2)
-    return a.difference(b)
+    arr1.sort()
+    arr2.sort()
+    idx = 0
+    while arr1[idx] == arr2[idx]:
+        idx += 1
+    return arr1[idx]
+
+t = TestFinder()
+t.test(finder)
